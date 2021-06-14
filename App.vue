33dedb805c480c9c8a77e404fa1e05603eb51a06
Josh Hansen <hansen.joshuaa@gmail.com>
2021-06-14 14:50:21 -0700

    Initial

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="app">Hi</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'App',
+	computed: {
+		showProgressPanel(): boolean {
+			return true;
+		},
+	},
+});
+</script>
